28938e6c6f618649b5fcd3283c2e886f02ef0c8e
isaacscardoso <isaacscardosoblue@outlook.com>
2023-09-16 20:55:29 -0300

    feat(index checkout): Implementation of the product detail list.

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -14,6 +14,20 @@
                     </dl>
                 </div>
 
+                <ul role="list" class="divide-y divide-white divide-opacity-10 text-sm font-medium">
+                    <li class="flex items-start space-x-4 py-6">
+                        <img class="h-20 w-20 flex-none rounded-md object-cover object-center"
+                             src="https://tailwindui.com/img/ecommerce-images/checkout-page-07-product-01.jpg"
+                             alt="Imagem do <nome_do_produto>"/>
+                        <div class="flex-auto space-y-1">
+                            <h3 class="text-white">Nome do Produto</h3>
+                            <p class="text-primary-200">Feature 1 do Produto</p>
+                            <p class="text-primary-200">Feature 2 do Produto</p>
+                        </div>
+                        <p class="flex-none text-base font-medium text-secondary-300">R$ 210,00</p>
+                    </li>
+                </ul>
+
             </section>
         </div>
     </div>
