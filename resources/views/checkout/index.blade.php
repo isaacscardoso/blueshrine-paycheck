14f8e8dcd57a34d14e2ba971760d72ec4afa91d0
isaacscardoso <isaacscardosoblue@outlook.com>
2023-09-16 20:24:28 -0300

    feat: Implementation of the right-hand section of the checkout index view.

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -2,5 +2,12 @@
     <div class="bg-tertiary-900">
         <div class="fixed left-0 top-0 hidden lg:block h-full w-1/2 bg-tertiary-900" aria-hidden="true"></div>
         <div class="fixed right-0 top-0 hidden lg:block h-full w-1/2 bg-tertiary-800" aria-hidden="true"></div>
+
+        <div class="relative mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2 gap-x-16 lg:px-8 lg:pt-16">
+            <section
+                aria-labelledby="summary-heading"
+                class="bg-tertiary-800 py-12 md:px-10 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:bg-transparent lg:px-0 lg:pb-24 lg:pt-0">
+            </section>
+        </div>
     </div>
 </x-app-layout>
