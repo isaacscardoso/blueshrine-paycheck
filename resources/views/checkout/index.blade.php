bc53e55ae9d9dd8b524d37303173030eea27ea01
isaacscardoso <isaacscardosoblue@outlook.com>
2023-09-16 19:15:22 -0300

    feat: Initial implementation of the 'index' containing two empty and fixed elements.

---
+++ resources/views/checkout/index.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <div class="bg-tertiary-900">
+        <div class="fixed left-0 top-0 hidden lg:block h-full w-1/2 bg-tertiary-900" aria-hidden="true"></div>
+        <div class="fixed right-0 top-0 hidden lg:block h-full w-1/2 bg-tertiary-800" aria-hidden="true"></div>
+    </div>
+</x-app-layout>
