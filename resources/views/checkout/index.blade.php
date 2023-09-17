e971c7db5a9e5859fd21a48a388ecbab9e41baeb
isaacscardoso <isaacscardosoblue@outlook.com>
2023-09-17 00:02:27 -0300

    feat(checkout index): Implementing the form section.

--- resources/views/checkout/index.blade.php
+++ resources/views/checkout/index.blade.php
@@ -4,6 +4,7 @@
         <div class="fixed right-0 top-0 hidden lg:block h-full w-1/2 bg-tertiary-800" aria-hidden="true"></div>
 
         <div class="relative mx-auto grid max-w-7xl grid-cols-1 lg:grid-cols-2 gap-x-16 lg:px-8 lg:pt-16">
+            <!--Summary-->
             <section
                 aria-labelledby="summary-heading"
                 class="bg-tertiary-800 py-12 md:px-10 lg:col-start-2 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:bg-transparent lg:px-0 lg:pb-24 lg:pt-0">
@@ -32,6 +33,30 @@
                         <x-checkout.summary-item title="Total" value="19.843,77" :is-last-div="true"/>
                     </dl>
                 </div>
+            </section>
+            <!--Formulary-->
+            <section
+                aria-labelledby="payment-and-shipping-heading"
+                class="py-16 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:w-full lg:max-w-lg lg:pb-24 lg:pt-0">
+
+                <form>
+                    <div class="mx-auto max-w-2xl px-4 lg:max-w-none lg:px-0">
+                        <div>
+                            <x-section-title title="Informações de Contato"/>
+                            <!--Email-->
+                            <div class="mt-6">
+                                <x-input-label for="email-address" value="E-mail"/>
+                                <x-text-input
+                                    type="email"
+                                    id="email-address"
+                                    name="email"
+                                    autocomplete="email"
+                                    placeholder="Digite o seu e-mail."
+                                    class="mt-2"/>
+                            </div>
+                        </div>
+                    </div>
+                </form>
             </section>
         </div>
     </div>
