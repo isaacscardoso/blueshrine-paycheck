7fb918f1b89de49be8c3ab306cac7b74a494c82f
isaacscardoso <isaacscardosoblue@outlook.com>
2023-10-01 02:39:28 -0300

    feat(OrderSku): Implementation of model, controller, factory, seeder, and migration of the 'order_sku' table.

---
+++ app/Http/Controllers/OrderSkuController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrderSku;
+use Illuminate\Http\Request;
+
+class OrderSkuController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(OrderSku $orderSku)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(OrderSku $orderSku)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, OrderSku $orderSku)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(OrderSku $orderSku)
+    {
+        //
+    }
+}
